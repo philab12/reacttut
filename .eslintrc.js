e02b2fb0710c11f297fb50852129b1c05835a953
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2022-02-10 09:34:30 +0100

    Change eslint config
    Disable no-console.
    Change JSX indent to 2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,11 +20,12 @@ module.exports = {
   plugins: ['prettier', 'react'],
   rules: {
     'prettier/prettier': ['error'],
+    'no-console': 'off',
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-indent-props': [2, 4],
-    'react/jsx-indent': [2, 4],
+    'react/jsx-indent': [2, 2],
     'react/jsx-one-expression-per-line': [0],
     'react/prefer-stateless-function': [1],
     'react/static-property-placement': [1, 'property assignment'],
